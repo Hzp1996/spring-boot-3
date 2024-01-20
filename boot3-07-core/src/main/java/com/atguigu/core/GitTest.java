b9c8949c0e4069a2ae408f0438553dd1c07cbd48
Hzp1996 <104967589+Hzp1996@users.noreply.github.com>
2024-01-20 16:47:16 +0800

    pull test

--- boot3-07-core/src/main/java/com/atguigu/core/GitTest.java
+++ boot3-07-core/src/main/java/com/atguigu/core/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.core;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("github test！");
+        System.out.println("pull test！");
     }
 }
